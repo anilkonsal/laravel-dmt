06451d710124aa77f5d2ff95e274c05cb8a51cc3
Anil Konsal <anil.konsal@sl.nsw.gov.au>
2016-09-09 16:46:10 +1000

    Repository code refactoring

--- database/repositories/ItemRepository.php
+++ database/repositories/ItemRepository.php
@@ -6,62 +6,78 @@ use App\Item;
 
 class ItemRepository {
 
-    public function getMastersCount()
+    const TYPE_STANDALONE = 'SA';
+    const TYPE_ALBUM = 'AL';
+
+    const REP_MASTER = 'masterRoot';
+    const REP_COMASTER = 'fromRoot';
+    const REP_HIRES = 'wroot';
+    const REP_STDRES = 'lroot';
+    const REP_PREVIEW = 'proot';
+    const REP_THUMBNAIL = 'troot';
+
+    protected $types = [
+        self::TYPE_STANDALONE   =>  [
+            'assetType' =>  'image',
+            'itemType'  =>  'image'
+        ],
+        self::TYPE_ALBUM    =>  [
+            'assetType' =>  'image',
+            'itemtype'  =>  'album'
+        ]
+    ];
+
+    protected $representations = [
+        self::REP_MASTER        =>  '_MASTER',
+        self::REP_COMASTER      =>  '_COMASTER',
+        self::REP_HIRES         =>  '_DAMx',
+        self::REP_STDRES        =>  '_DAMl',
+        self::REP_PREVIEW       =>  '_DAMp',
+        self::REP_THUMBNAIL     =>  '_DAMt',
+    ];
+
+    public function getMastersCount($type = self::TYPE_STANDALONE)
     {
-        $count = \DB::table('item')
-                ->where('assetType', 'image')
-                ->where('itemType', 'image')
-                ->where('masterRoot', 'like', '_MASTER%')
-                ->count();
+        $count = $this->_getCount($type, self::REP_MASTER);
         return $count;
     }
 
-    public function getComastersCount()
+    public function getComastersCount($type = self::TYPE_STANDALONE)
     {
-        $count = \DB::table('item')
-                ->where('assetType', 'image')
-                ->where('itemType', 'image')
-                ->where('fromRoot', 'like', '_COMASTER%')
-                ->count();
+        $count = $this->_getCount($type, self::REP_COMASTER);
         return $count;
     }
 
-    public function getHiresCount()
+    public function getHiresCount($type = self::TYPE_STANDALONE)
     {
-        $count = \DB::table('item')
-                ->where('assetType', 'image')
-                ->where('itemType', 'image')
-                ->where('wroot', 'like', '_DAMx%')
-                ->count();
+        $count = $this->_getCount($type, self::REP_HIRES);
         return $count;
     }
 
-    public function getStdresCount()
+    public function getStdresCount($type = self::TYPE_STANDALONE)
     {
-        $count = \DB::table('item')
-                ->where('assetType', 'image')
-                ->where('itemType', 'image')
-                ->where('lroot', 'like', '_DAMl%')
-                ->count();
+        $count = $this->_getCount($type, self::REP_STDRES);
         return $count;
     }
 
-    public function getPreviewCount()
+    public function getPreviewCount($type = self::TYPE_STANDALONE)
     {
-        $count = \DB::table('item')
-                ->where('assetType', 'image')
-                ->where('itemType', 'image')
-                ->where('proot', 'like', '_DAMp%')
-                ->count();
+        $count = $this->_getCount($type, self::REP_PREVIEW);
         return $count;
     }
 
-    public function getThumbnailCount()
+    public function getThumbnailCount($type = self::TYPE_STANDALONE)
     {
+        $count = $this->_getCount($type, self::REP_THUMBNAIL);
+        return $count;
+    }
+
+    protected function _getCount($type, $representation) {
         $count = \DB::table('item')
-                ->where('assetType', 'image')
-                ->where('itemType', 'image')
-                ->where('troot', 'like', '_DAMt%')
+                ->where('assetType', $this->types[$type]['assetType'])
+                ->where('itemType', $this->types[$type]['itemType'])
+                ->where($representation, 'like', $this->representations[$representation].'%')
+                ->where('status','<>','rejected')
                 ->count();
         return $count;
     }
