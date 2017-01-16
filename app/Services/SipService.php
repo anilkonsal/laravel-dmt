34f0d9076ceda73157ef76c9200dc54d72036a7f
Anil Konsal <anil.konsal@sl.nsw.gov.au>
2017-01-16 11:12:32 +1100

    Changes made for puting the files in Rep Folders rather than zipping

--- app/Services/SipService.php
+++ app/Services/SipService.php
@@ -430,7 +430,7 @@ class SipService {
                 if (!file_exists($repFolder)) {
                     mkdir($repFolder, 2775);
                 }
-                copy($filePath, $repFolder.'/'.$fileName);
+                copy($filePath, dirname($repFolder).'/'.$fileName);
             }
         }
         return true;
